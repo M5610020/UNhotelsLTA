2167c07a899fb11d5e515407fe8989f6e76213b7
M5610020 <43448202+M5610020@users.noreply.github.com>
2023-04-26 15:15:59 +0600

    changed symbology pop-up 3

--- js/qgis2web_expressions.js
+++ js/qgis2web_expressions.js
@@ -754,17 +754,3 @@ function fnc__specialcol_(values, context) {
 function fnc_project_color(values, context) {
     return false;
 };
-
-
-
-function exp_HotelLTA_3rule0_eval_expression(context) {
-    // "Districts" IS NOT NULL
-
-    var feature = context.feature;
-    
-    if (feature.properties) {
-        return (feature.properties['Districts']  !== null);
-    } else {
-        return (feature['Districts']  !== null);
-    }
-}
